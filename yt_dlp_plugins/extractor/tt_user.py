0efee3710b7f34a30b36aaf8a98ab2b796cbd1a1
924nf9sh33nf <151471800+924nf9sh33nf@users.noreply.github.com>
2023-12-07 22:39:18 -0800

    Update tt_user.py

--- yt_dlp_plugins/extractor/tt_user.py
+++ yt_dlp_plugins/extractor/tt_user.py
@@ -86,7 +86,7 @@ class TikTokUser_TTUserIE(TikTokUserIE, plugin_name='TTUser'):
                     'track': "",
                     'album': "",
                     'artist': "",
-                    'formats': ["formats"],
+                    'formats': [{}],
                     'subtitles': "",
                     'thumbnails': "",
                     'duration': 10,
