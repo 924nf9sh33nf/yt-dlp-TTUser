80ac3d1b2908f0f11df6e71f666b16837dfc9a56
924nf9sh33nf <151471800+924nf9sh33nf@users.noreply.github.com>
2023-12-07 21:26:18 -0800

    Update tt_user.py

--- yt_dlp_plugins/extractor/tt_user.py
+++ yt_dlp_plugins/extractor/tt_user.py
@@ -77,23 +77,13 @@ class TikTokUser_TTUserIE(TikTokUserIE, plugin_name='TTUser'):
                     yield self.url_result(self._create_url(user_name, video_id), TikTokIE, video_id)
                     continue
 
-                entry = {}
-                try:
-                    entry = self._extract_aweme_app(video_id)
-                except ExtractorError as e:
-                    self.report_warning(
-                        f'{e.orig_msg}. Failed to extract from feed; falling back to web API response')
-                    if traverse_obj(video, ('video', 'playAddr')):
-                        entry = self._parse_aweme_video_web(video, self._create_url(user_name, video_id), video_id)
-                if entry:
-                    yield {
-                        **entry,
-                        'extractor_key': TikTokIE.ie_key(),
-                        'extractor': 'TikTok',
-                        'webpage_url': self._create_url(user_name, video_id),
-                    }
-                else:
-                    self.report_warning(f'Unable to extract video {video_id}')
+                entry = { video_id }
+                yield {
+                    **entry,
+                    'extractor_key': TikTokIE.ie_key(),
+                    'extractor': 'TikTok',
+                    'webpage_url': self._create_url(user_name, video_id),
+                }
 
             old_cursor = cursor
             cursor = traverse_obj(
